daa67da023e4862d9143b3a8411103b2534b818e
Cristiano Roberto da Silva <javacristiano2021@gmail.com>
2022-06-26 13:52:12 -0300

    Escrever e Leitura de Arquivo com csv

---
+++ src/entidades/Produto.java
@@ -0,0 +1,40 @@
+package entidades;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+
+	private String nome;
+	private Double preco;
+	private Integer quantidade;
+	public Produto(String nome, Double preco, Integer quantidade) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	@Override
+	public String toString() {
+		return "Produto [nome=" + nome + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+	}
+
+	
+}
