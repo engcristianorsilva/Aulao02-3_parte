655f737257d9fa21801c0493f7a160b2ef5899ac
Cristiano Roberto da Silva <javacristiano2021@gmail.com>
2022-06-28 12:41:14 -0300

    versão com leitura de arquivo cvs com sucesso

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,44 +1,54 @@
 package aplicacao;
 
+//versão 3 é a melhor!
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 import entidades.Produto;
 
 public class Programa {
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US); // tipo formatação numérica
+		
+		String path = "C:\\emporio_testes2.csv";
 
-		String path = "C:\\emporio.txt";
-		
 		List<Produto> list = new ArrayList<Produto>();
-		
+
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			
+
 			String line = br.readLine();
 			line = br.readLine();
 			while (line != null) {
-				
+
 				String[] vect = line.split(",");
-				String nome = vect[0];
-				Double preco = Double.parseDouble(vect[1]);
-				Integer quantidade = Integer.parseInt(vect[2]);
 				
-				Produto prod = new Produto(nome, preco, quantidade);
+				int idProduto = Integer.parseInt(vect[0]);
+				
+				String produto = vect[1];
+				
+				double preco = Double.parseDouble(vect[2]);
+				
+				
+				
+				Produto prod = new Produto(idProduto,produto, preco);
 				list.add(prod);
-				
+
 				line = br.readLine();
-			}	
-			
+			}
+
 			System.out.println("PRODUTOS:");
 			for (Produto p : list) {
 				System.out.println(p);
 			}
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 	}
--- src/entidades/Produto.java
+++ src/entidades/Produto.java
@@ -4,37 +4,43 @@ import java.io.Serializable;
 
 public class Produto implements Serializable {
 
-	private String nome;
-	private Double preco;
-	private Integer quantidade;
-	public Produto(String nome, Double preco, Integer quantidade) {
+	private int idProduto;
+	private String produto;
+	private double preco;
+	public Produto(int idProduto, String produto, double preco) {
 		super();
-		this.nome = nome;
+		this.idProduto = idProduto;
+		this.produto = produto;
 		this.preco = preco;
-		this.quantidade = quantidade;
 	}
-	public String getNome() {
-		return nome;
+	public int getIdProduto() {
+		return idProduto;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
 	}
-	public Double getPreco() {
+	public String getProduto() {
+		return produto;
+	}
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+	public double getPreco() {
 		return preco;
 	}
-	public void setPreco(Double preco) {
+	public void setPreco(double preco) {
 		this.preco = preco;
-	}
-	public Integer getQuantidade() {
-		return quantidade;
-	}
-	public void setQuantidade(Integer quantidade) {
-		this.quantidade = quantidade;
 	}
 	@Override
 	public String toString() {
-		return "Produto [nome=" + nome + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+		return "Produto [idProduto=" + idProduto + ", produto=" + produto + ", preco=" + preco + "]";
 	}
+	
+	
+	
+	}
+	
+	
 
 	
-}
+
